9edd3a3a5ad4859046515efa32f89cde021066b4
dudolad_yaroslav <dudolad2009@gmail.com>
2025-08-29 16:13:37 +0300

    first exercise

--- HW4_OOP/HW4_OOP.cpp
+++ HW4_OOP/HW4_OOP.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+ 	char C = 0;
+ 	string S, S0;
+    cout << "Enter a character: ";
+ 	cin >> C;
+ 	cout << "Enter a string: ";
+    getline(cin, S);
+    cout << "Enter a substring: ";
+    cin >> S0;
+ 	for (int i = 0; i < S.length(); i++)
+ 	{
+ 		if (S[i] == C) {
+ 			S.insert(i, S0);
+ 			i += S0.length();
+ 		}
+ 	}
+ 	cout << "Result: " << S << endl;
+ 
+ 	return 0;
 }
