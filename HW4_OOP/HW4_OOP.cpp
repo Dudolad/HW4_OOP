319b6d73c0106f77e82603d1da0854951cca84b9
dudolad_yaroslav <dudolad2009@gmail.com>
2025-08-29 16:30:58 +0300

    exercise 3

--- HW4_OOP/HW4_OOP.cpp
+++ HW4_OOP/HW4_OOP.cpp
@@ -26,25 +26,45 @@ using namespace std;
 // 	return 0;
 //}   
 
+//int main()
+//{
+//	char C = 0;
+//		string S, S0;
+//	    cout << "Enter a character: ";
+//	 	cin >> C;
+//	 	cout << "Enter a string: ";
+//		cin.ignore();
+//	    getline(cin, S);
+//	    cout << "Enter a substring: ";
+//	    cin >> S0;
+//	 	for (int i = 0; i < S.length(); i++)
+//	 	{
+//	 		if (S[i] == C) {
+//	 			S.insert(i+1, S0);
+//	 			i += S0.length();
+//	 		}
+//	 	}
+//	 	cout << "Result: " << S << endl;
+//	 
+//	 	return 0;
+//}
+
 int main()
 {
-	char C = 0;
-		string S, S0;
-	    cout << "Enter a character: ";
-	 	cin >> C;
-	 	cout << "Enter a string: ";
-		cin.ignore();
-	    getline(cin, S);
-	    cout << "Enter a substring: ";
-	    cin >> S0;
-	 	for (int i = 0; i < S.length(); i++)
-	 	{
-	 		if (S[i] == C) {
-	 			S.insert(i+1, S0);
-	 			i += S0.length();
-	 		}
-	 	}
-	 	cout << "Result: " << S << endl;
-	 
-	 	return 0;
+	string S, S0;
+		cout << "Enter a string: ";
+		getline(cin, S);
+		cout << "Enter a substring: ";
+		cin >> S0;
+
+		int count = 0;	
+		int pos = S.find(S0);
+	
+		
+		while (pos != -1) {
+			count++;
+			pos = S.find(S0, pos + S0.length());
+		}
+		cout << "Count: " << count << endl;
+		return 0;
 }
