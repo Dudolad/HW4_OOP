fd5dd726f2ef5d8a1b71b5bb32a3e6ddd5cc9909
dudolad_yaroslav <dudolad2009@gmail.com>
2025-08-29 16:23:38 +0300

    exercise 2

--- HW4_OOP/HW4_OOP.cpp
+++ HW4_OOP/HW4_OOP.cpp
@@ -3,25 +3,48 @@
 #include <fstream>
 using namespace std;
 
+//int main()
+//{
+// 	char C = 0;
+//	string S, S0;
+//    cout << "Enter a character: ";
+// 	cin >> C;
+// 	cout << "Enter a string: ";
+//	cin.ignore();
+//    getline(cin, S);
+//    cout << "Enter a substring: ";
+//    cin >> S0;
+// 	for (int i = 0; i < S.length(); i++)
+// 	{
+// 		if (S[i] == C) {
+// 			S.insert(i, S0);
+// 			i += S0.length();
+// 		}
+// 	}
+// 	cout << "Result: " << S << endl;
+// 
+// 	return 0;
+//}   
+
 int main()
 {
- 	char C = 0;
-	string S, S0;
-    cout << "Enter a character: ";
- 	cin >> C;
- 	cout << "Enter a string: ";
-	cin.ignore();
-    getline(cin, S);
-    cout << "Enter a substring: ";
-    cin >> S0;
- 	for (int i = 0; i < S.length(); i++)
- 	{
- 		if (S[i] == C) {
- 			S.insert(i, S0);
- 			i += S0.length();
- 		}
- 	}
- 	cout << "Result: " << S << endl;
- 
- 	return 0;
+	char C = 0;
+		string S, S0;
+	    cout << "Enter a character: ";
+	 	cin >> C;
+	 	cout << "Enter a string: ";
+		cin.ignore();
+	    getline(cin, S);
+	    cout << "Enter a substring: ";
+	    cin >> S0;
+	 	for (int i = 0; i < S.length(); i++)
+	 	{
+	 		if (S[i] == C) {
+	 			S.insert(i+1, S0);
+	 			i += S0.length();
+	 		}
+	 	}
+	 	cout << "Result: " << S << endl;
+	 
+	 	return 0;
 }
